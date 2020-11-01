a3ba5d884cca6e1f657ae436cea5198747609070
shikharani8 <shikharani8@gmail.com>
2020-11-01 18:05:08 +0000

    Debugger made off

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dr2c5k&)8uz#93!_-2x*xxipr$#22u9wdm#u&%b#v^ai7wtx$c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shikharani8.pythonanywhere.com']
 
